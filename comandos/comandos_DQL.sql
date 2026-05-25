3ec2d7eed336f021bead321fcd326596ff7811db
Geovany de Oliveira Silva Batista <2025116TADS0033@ifpi.int>
2026-05-25 17:08:47 -0300

    .

--- comandos/comandos_DQL.sql
+++ comandos/comandos_DQL.sql
@@ -19,3 +19,5 @@ SELECT * FROM quartos WHERE status LIKE 'Livre';
 SELECT nome,data_nascimento FROM hospedes WHERE nome ILIKE '%silva%' ORDER BY data_nascimento ASC;
 
 SELECT * FROM hospedes WHERE  data_nascimento>= '1970-01-01' AND data_nascimento<='1979-12-31';
+
+SELECT * FROM hospedes WHERE  data_nascimento BETWEEN '1970-01-01' AND'1979-12-31';
