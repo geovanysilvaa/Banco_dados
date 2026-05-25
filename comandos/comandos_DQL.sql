5afa44eebc1c0ffd10cfb9d31817b175f4cede94
Geovany de Oliveira Silva Batista <2025116TADS0033@ifpi.int>
2026-05-25 17:04:35 -0300

    .

---
+++ comandos/comandos_DQL.sql
@@ -0,0 +1,21 @@
+SELECT 
+numero,
+tipo, 
+valor_diaria,
+valor_diaria * 7 * 0.9 AS "valor semanual" FROM quartos;
+
+
+SELECT 
+numero,
+tipo, 
+valor_diaria,
+valor_diaria * 7 * 0.9 AS "valor semanual"
+FROM quartos;
+
+
+SELECT * FROM quartos WHERE status = 'Livre';
+SELECT * FROM quartos WHERE status LIKE 'Livre';
+
+SELECT nome,data_nascimento FROM hospedes WHERE nome ILIKE '%silva%' ORDER BY data_nascimento ASC;
+
+SELECT * FROM hospedes WHERE  data_nascimento>= '1970-01-01' AND data_nascimento<='1979-12-31';
